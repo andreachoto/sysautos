4da3fb3ffed6eaa6aeab0dfddcc15c7390bcfb65
hp <hp@Pavilion-dm4>
2015-06-28 12:23:57 -0500

    nuevas correciones

--- src/java/sysautos/integration/Global.java
+++ src/java/sysautos/integration/Global.java
@@ -9,8 +9,8 @@ package sysautos.integration;
  * @author hp
  */
 public class Global {
-    public static final String URL = "jdbc:postgresql://localhost:5432/sysautosbd";
+    public static final String URL = "jdbc:postgresql://localhost:5432/autosdb";
     public static final String USER = "postgres";
-    public static final String PASSWORD = "123456";
+    public static final String PASSWORD = "admin";
     public static final String DRIVER = "org.postgresql.Driver";
 }
