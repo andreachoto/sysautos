de2f80f1fc18090015869063286d97061e2be3f4
andrea <andrea@ProBook4530>
2015-07-15 07:31:33 +0200

    Cred

--- src/java/sysautos/bussines/controllers/vmbRecli.java
+++ src/java/sysautos/bussines/controllers/vmbRecli.java
@@ -6,24 +6,130 @@
 package sysautos.bussines.controllers;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import sysautos.bussines.drivers.dvrRequisitosCliente;
+import sysautos.bussines.entities.ClienteCredito;
+import sysautos.bussines.entities.RequisitosCliente;
+import sysautos.bussines.session.MbsMessages;
 
 /**
  *
  * @author DISMAGIC0985043396
  */
-@ManagedBean
+@ManagedBean(name = "dtReqView")
 @ViewScoped
 public final class vmbRecli implements Serializable {
 
-    /**
-     * Creates a new instance of vmbRecli
-     */
+    private static List<RequisitosCliente> listaReqCliente;
+    private RequisitosCliente reqcliNuevo;
+    private RequisitosCliente reqcliSel;
+    private int codigo;
+    private ClienteCredito objclientecred;
+    private List<RequisitosCliente> lstReqCli;
+    
+   
+    
     public vmbRecli() {
+          this.reqcliNuevo= new RequisitosCliente();
+          this.objclientecred= new ClienteCredito();
+          
+          
+          this.loadreqcli();
     }
+    
+     public void loadreqcli() {
+        try {
+            this.listaReqCliente = dvrRequisitosCliente.getRequisitosClienteById(codigo);
+            
+        } catch (Exception ex) {
+            MbsMessages.fatal(ex.getMessage());
+        }
+    }
+
+    public List<RequisitosCliente> getLstReqCli() {
+        return lstReqCli;
+    }
+
+    public void setLstReqCli(List<RequisitosCliente> lstReqCli) {
+        this.lstReqCli = lstReqCli;
+    }
+
+    public List<RequisitosCliente> getListaReqCliente() {
+        return listaReqCliente;
+    }
+
+    public void setListaReqCliente(List<RequisitosCliente> listaReqCliente) {
+        this.listaReqCliente = listaReqCliente;
+    }
+
+    public RequisitosCliente getReqcliNuevo() {
+        return reqcliNuevo;
+    }
+
+    public void setReqcliNuevo(RequisitosCliente reqcliNuevo) {
+        this.reqcliNuevo = reqcliNuevo;
+    }
+
+    public RequisitosCliente getReqcliSel() {
+        return reqcliSel;
+    }
+
+    public void setReqcliSel(RequisitosCliente reqcliSel) {
+        this.reqcliSel = reqcliSel;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public ClienteCredito getObjclientecred() {
+        return objclientecred;
+    }
+
+    public void setObjclientecred(ClienteCredito objclientecred) {
+        this.objclientecred = objclientecred;
+    }
+     
+    public void buscarreq() throws Exception {
+
+        Integer cliente = objclientecred.getIdcli();
+        Integer credito = objclientecred.getIdcred();
+        lstReqCli = dvrRequisitosCliente.getRequisitosClienteByClCr(cliente, credito);
+                 
+    }
+     
+     
+     
+     
+     
+     
+     
+     
+     
+//      public void loadreclientes(RequisitosCliente tipo) {
+//        try {
+//            if (tipo != null){
+//                this.reqcliSel = dvrRequisitosCliente.getRequisitosClienteById(tipo.getCltid());
+//                RequestContext.getCurrentInstance().update("frmEditRemopa");
+//                RequestContext.getCurrentInstance().execute("PF('editremopa').show()");
+//            } else {
+//                MbsMessages.error("Seleccione un registro");
+//            }
+//        } catch (Exception ex) {
+//            MbsMessages.fatal(ex.getMessage());
+//        }
+//    }
+//      
+      
     
     
     
     
 }
+
--- src/java/sysautos/bussines/controllers/vmbclicre.java
+++ src/java/sysautos/bussines/controllers/vmbclicre.java
@@ -6,13 +6,30 @@
 package sysautos.bussines.controllers;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import org.primefaces.context.DefaultRequestContext;
+import org.primefaces.context.RequestContext;
+import sysautos.bussines.drivers.dvrAmortizacion;
 import sysautos.bussines.drivers.dvrClienteCredito;
+import sysautos.bussines.drivers.dvrCredit;
+import sysautos.bussines.drivers.dvrRequisitosCliente;
+import sysautos.bussines.drivers.dvrTipodeudor;
+import sysautos.bussines.entities.Amortizacion;
 import sysautos.bussines.entities.ClienteCredito;
+import sysautos.bussines.entities.Credit;
+import sysautos.bussines.entities.RequisitosCliente;
+import sysautos.bussines.entities.Tipodeudor;
 import sysautos.bussines.session.MbsMessages;
 
 /**
@@ -23,26 +40,66 @@ import sysautos.bussines.session.MbsMessages;
 @ViewScoped
 public final class vmbclicre implements Serializable {
 
+    
+
     private List<ClienteCredito> listaClienteCredito;
     private ClienteCredito objclientecred;
     private ClienteCredito objsel;
-    public vmbclicre() throws Exception {
-        
-        this.objclientecred= new ClienteCredito();
+    private Credit credito;
+    private List<ClienteCredito> lstCredito;
+    private List<RequisitosCliente> lstReqCli;
+    private RequisitosCliente objreqcli;
+
+    
+    
+    public vmbclicre() throws Exception { 
+
+        this.objclientecred = new ClienteCredito();
         this.loadclicreditos();
-        
+
     }
-    
-    
+
     public void loadclicreditos() {
         try {
             this.listaClienteCredito = dvrClienteCredito.getAplicarList();
         } catch (Exception ex) {
             MbsMessages.fatal(ex.getMessage());
         }
-        
-        objclientecred= new ClienteCredito();
-        objsel=new ClienteCredito();
+
+        objclientecred = new ClienteCredito();
+        objsel = new ClienteCredito();
+    }
+
+    public RequisitosCliente getObjreqcli() {
+        return objreqcli;
+    }
+
+    public void setObjreqcli(RequisitosCliente objreqcli) {
+        this.objreqcli = objreqcli;
+    }
+
+    
+   public List<RequisitosCliente> getLstReqCli() {
+        return lstReqCli;
+    }
+
+    public void setLstReqCli(List<RequisitosCliente> lstReqCli) {
+        this.lstReqCli = lstReqCli;
+    }
+    public Credit getCredito() {
+        return credito;
+    }
+
+    public void setCredito(Credit credito) {
+        this.credito = credito;
+    }
+
+    public List<ClienteCredito> getLstCredito() {
+        return lstCredito;
+    }
+
+    public void setLstCredito(List<ClienteCredito> lstCredito) {
+        this.lstCredito = lstCredito;
     }
 
     public List<ClienteCredito> getListaClienteCredito() {
@@ -68,7 +125,41 @@ public final class vmbclicre implements Serializable {
     public void setObjsel(ClienteCredito objsel) {
         this.objsel = objsel;
     }
+
+    public void buscar() throws Exception {
+
+        Integer numero = objclientecred.getIdcred();
+        lstCredito = dvrRequisitosCliente.getClientesListByCrdito(numero);
+
+    }
+     public void buscarreq() throws Exception {
+
+        Integer cliente = objclientecred.getIdcli();
+        Integer credito1 = objclientecred.getIdcred();
+        lstReqCli = dvrRequisitosCliente.getRequisitosClienteByClCr(cliente, credito1);
+                 
+    }
     
+    
+    
+    
+       public void loadclicreditos(ClienteCredito tipo) {
+        try {
+            if (tipo != null){
+               // this.objsel = dvrClienteCredito.getClientecreditoById(tipo.getIdcli(),tipo.getIdcred());
+//                RequestContext.getCurrentInstance().update("frmEditRemopa");
+              
+                 lstReqCli = dvrRequisitosCliente.getRequisitosClienteByClCr(tipo.getIdcli(), tipo.getIdcred());
+                 RequestContext.getCurrentInstance().execute("PF('editremopa').show()");
+            } else {
+                MbsMessages.error("Seleccione un registro");
+            }
+        } catch (Exception ex) {
+            MbsMessages.fatal(ex.getMessage());
+        }
+    }
+    
+
     public void insertar() {
         try {
 //            if ((dvrClienteCredito.ClienteCreditoRegister(objsel))==true) {
@@ -89,5 +180,4 @@ public final class vmbclicre implements Serializable {
         }
     }
 
-    
 }
